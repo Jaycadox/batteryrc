4e4728fffbbe36f44c489468b7d459c08f0d9b66
Jaycadox <jaycadox@example.com>
2024-02-07 14:27:00 +1000

    refactor code to make errors result in crashes less frequently

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,57 @@ use std::{error::Error, process::Command};
 use directories::ProjectDirs;
 use systemstat::{Duration, Platform};
 
+struct ShellCommand {
+    name: String,
+    args: Vec<String>,
+}
+
+impl TryFrom<&str> for ShellCommand {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let Ok(value) = shellwords::split(value) else {
+            eprintln!("Unable to parse shell command");
+            return Err("Failed shell command parse".into());
+        };
+        let Some(name) = value.first() else {
+            eprintln!("Could not find name for shell command: {value:?}");
+            return Err("Invalid shell command".into());
+        };
+        let name = name.to_string();
+
+        let args = value.get(1..).map(|x| x.to_vec()).unwrap_or_default();
+
+        Ok(Self { name, args })
+    }
+}
+
+impl ShellCommand {
+    fn to_command(&self) -> Command {
+        let command_name = &self.name;
+        let command_args = &self.args;
+
+        let mut command = Command::new(command_name);
+        if !command_args.is_empty() {
+            command.args(command_args);
+        }
+
+        command
+    }
+}
+
 struct Config {
-    on_ac_cmds: Vec<String>,
-    on_bat_cmds: Vec<String>,
+    on_ac_cmds: Vec<ShellCommand>,
+    on_bat_cmds: Vec<ShellCommand>,
 }
 
 impl Config {
-    fn parse_config(
-        config: &str,
-        ac_cmds: &mut Vec<String>,
-        bat_cmds: &mut Vec<String>,
-    ) -> Result<(), Box<dyn Error>> {
+    fn parse_config(config_text: &str) -> Result<Self, Box<dyn Error>> {
+        let mut config = Self {
+            on_ac_cmds: vec![],
+            on_bat_cmds: vec![],
+        };
+
         enum ParseMode {
             None,
             Battery,
@@ -21,42 +61,57 @@ impl Config {
         }
 
         let mut parse_mode = ParseMode::None;
-        let lines = config.lines().map(|l| l.trim()).collect::<Vec<&str>>();
+        let lines = config_text
+            .lines()
+            .map(|l| l.trim())
+            .filter(|l| !l.is_empty())
+            .collect::<Vec<&str>>();
 
         for line in lines {
-            match line {
+            match &*line.to_lowercase() {
                 "@battery" => parse_mode = ParseMode::Battery,
                 "@ac" => parse_mode = ParseMode::Ac,
                 x => match parse_mode {
-                    ParseMode::Battery => bat_cmds.push(x.to_string()),
-                    ParseMode::Ac => ac_cmds.push(x.to_string()),
+                    ParseMode::Battery => {
+                        config.on_bat_cmds.push(match ShellCommand::try_from(x) {
+                            Ok(content) => content,
+                            Err(e) => {
+                                eprintln!(
+                                    "Error while parsing shell command for battery state. {e}"
+                                );
+                                continue;
+                            }
+                        })
+                    }
+                    ParseMode::Ac => config.on_ac_cmds.push(match ShellCommand::try_from(x) {
+                        Ok(content) => content,
+                        Err(e) => {
+                            eprintln!("Error while parsing shell command for AC state. {e}");
+                            continue;
+                        }
+                    }),
                     ParseMode::None => {
-                        return Err("Attempted to specify command without valid parse mode".into())
+                        eprintln!("Attempted to specify command without valid parse mode");
+                        continue;
                     }
                 },
             }
         }
 
-        Ok(())
+        Ok(config)
     }
 
     pub fn try_new() -> Result<Config, Box<dyn Error>> {
         if let Some(dirs) = ProjectDirs::from("io.github", "Jaycadox", "batteryrc") {
             let config = dirs.config_dir();
             if !std::path::Path::exists(config) {
-                std::fs::create_dir_all(config)?;
+                std::fs::create_dir_all(config)?; // I suppose if the directory fails to be
+                                                  // created, the user has bigger problems.
             }
             let config = config.join(".batteryrc");
             let config_contents = std::fs::read_to_string(config)?;
 
-            let mut ac_cmds = vec![];
-            let mut bat_cmds = vec![];
-            Self::parse_config(&config_contents, &mut ac_cmds, &mut bat_cmds)?;
-
-            return Ok(Self {
-                on_ac_cmds: ac_cmds,
-                on_bat_cmds: bat_cmds,
-            });
+            return Self::parse_config(&config_contents);
         }
         Err("unable to find config path".into())
     }
@@ -69,32 +124,58 @@ fn power_status_changed(config: &Config, is_on_ac: bool) -> Result<(), Box<dyn E
         &config.on_bat_cmds
     };
 
-    for command in commands {
-        if command.trim().is_empty() {
-            continue;
-        }
+    let mut commands = commands
+        .iter()
+        .map(|cmd| cmd.to_command())
+        .collect::<Vec<_>>();
 
-        let command_parts = shellwords::split(command)?;
-        let mut command = Command::new(command_parts.first().expect("Command does not have name"));
-        if command_parts.len() > 1 {
-            command.args(&command_parts[1..]);
-        }
-
-        let _ = command.status()?;
+    dbg!("Battery status changed. On AC = {is_on_ac}.");
+    dbg!("Running {} saved commands...", commands.len());
+    for command in commands.iter_mut() {
+        dbg!("> {}", &command);
+        if command.status().is_err() {
+            eprintln!("Command ran with a failed exit code");
+        };
     }
 
     Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut args = std::env::args().skip(1);
+    let first_arg = args.next();
+
+    if let Some(first_arg) = first_arg {
+        match &*first_arg {
+            "--help" | "-h" => {
+                println!("BatteryRC -- made by jayphen");
+                return Ok(());
+            }
+            _ => {}
+        };
+    }
+
     let sys = systemstat::System::new();
     let mut on_ac_power = !sys.on_ac_power()?; // Inverted so the first iteration will run
 
+    if let Err(e) = Config::try_new() {
+        // If the config initially fails to load, we probably want to fail quickly
+        eprintln!("Failed to load initial configuration!");
+        eprintln!("{e}");
+        return Ok(());
+    }
+
     loop {
-        let now_on_ac_power = sys.on_ac_power()?;
+        let Ok(now_on_ac_power) = sys.on_ac_power() else {
+            eprintln!("Failed to retrieve battery status");
+            continue;
+        };
 
         if now_on_ac_power != on_ac_power {
-            let config = Config::try_new()?;
+            let Ok(config) = Config::try_new() else {
+                eprintln!("Failed to parse shell configuration");
+                continue;
+            };
             power_status_changed(&config, now_on_ac_power)?;
         }
 
